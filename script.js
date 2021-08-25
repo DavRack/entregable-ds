80f8053b09303759cff84291718e3df5d8a84bfc
david <dlondono@vertical.com.co>
2021-08-24 22:16:12 -0500

    update

---
+++ script.js
@@ -0,0 +1,14 @@
+function changePage(page){
+    switch(page){
+        case 'vehiculo':
+            window.location.replace("vehiculo.html");
+            break
+        case 'viaje':
+            window.location.replace("viaje.html");
+            break
+        case 'calificar':
+            window.location.replace("calificar.html");
+            break
+
+    }
+}
