45c946516a733545f03579f0fe23e96cd79e4cd2
Mateo Martínez Palacio <matmartinezpal@unal.edu.co>
2021-08-29 22:46:42 -0500

    Update Viaje.js

--- src/controllers/RegistrarVehiculoControlador.js
+++ src/controllers/RegistrarVehiculoControlador.js
@@ -66,7 +66,7 @@ buttonRegister.addEventListener("click", function() {
     }
 
     let jsonData = {
-        cedula_conductor: JSON.parse(localStorage.getItem("conductor"))["cedula"],
+        cedula_conductor: JSON.parse(localStorage.getItem("conductor1"))["cedula"],
         placa: placa.value,
         capacidad: capacity.textContent,
         modelo: modelo.value,
